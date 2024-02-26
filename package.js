f43b143f980a41baf3da221632b446d99ac920db
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2024-02-26 13:05:05 -0300

    publish 1.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     git: "https://github.com/henriquealbert/meteor-settings-validator",
     name: "hschmaiske:meteor-settings-validator",
-    version: "1.0.0",
+    version: "1.0.1",
     summary: "Meteor Settings e2e type safe",
     documentation: "README.md",
 });
@@ -13,7 +13,7 @@ Npm.depends({
 Package.onUse(function (api) {
     api.versionsFrom(["2.13.3", "3.0-beta.4"]);
     api.use("ecmascript");
-    api.use("typescript");
-    api.use("zodern:types@1.0.0", { weak: true });
+    api.use(["typescript@3.0.0||4.0.0||5.0.0||6.0.0||7.0.0||5.3.3-beta300.4"]);
+    api.use("zodern:types@1.0.0");
     api.mainModule("src/index.ts");
 });
